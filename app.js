794113df0cf04d3c18c199e517a2e6fad2e5b324
Brendan Koral <brendanhkoral@gmail.com>
2016-08-26 11:28:47 -0500

    Show weather data in console

--- app.js
+++ app.js
@@ -3,5 +3,28 @@
 searchButton.addEventListener('click', searchWeather);
 
 function searchWeather() {
-    console.log(searchCity.value);
+    var cityName = searchCity.value;
+    if (cityName.trim().length == 0) {
+        return alert("Please enter a city name");
+    }
+    var http = new XMLHttpRequest();
+    var apiKey = '8755b3bed12bb3e4954c80101559d1de';
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
+    var method = 'GET';
+
+    http.open(method, url);
+    http.onreadystatechange = function () {
+        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+            var data = JSON.parse(http.responseText);
+            //Parse JSON data and turn it into a JavaScript object
+
+            var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+            weatherData.temperature = data.main.temp;
+            console.log(weatherData);
+
+        } else if (http.readyState === XMLHttpRequest.DONE) {
+            alert("Something went wrong");
+        }
+    };
+    http.send();
 }
--- weather-data.js
+++ weather-data.js
@@ -4,6 +4,8 @@ function Weather(cityName, description) {
     this.cityName = cityName;
     this.description = description;
     this._temperature = '';
+    //Underscore is a private field. Not passed into the constructor
+    //Want to set up a GETer and SETer to guarantee whatever's passed here is converted to Farenheit or Celsius
 }
 
 Object.defineProperty(Weather.prototype, 'temperature', {
